cc463ab5a63b857e40830479537b7ef41d4721cc
Jamie Sewell <jamiegsewell@gmail.com>
2025-06-11 11:10:09 +0000

    feat: prepares for deployment

--- book_reviews_api/settings.py
+++ book_reviews_api/settings.py
@@ -43,6 +43,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'book_reviews_api.serializers.CurrentUserSerializer'}
 
@@ -50,12 +51,12 @@ REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'book_reviews_api.serializer
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^)g61ls7tq76ur_9@en*&x7&^_$as7((rvgd(ji7^g#s+t9r2'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-jamies2000-bookreviewsa-5mviu39cob8.ws-eu120.gitpod.io']
+ALLOWED_HOSTS = ['8000-jamies2000-bookreviewsa-5mviu39cob8.ws-eu120.gitpod.io', 'book-review-api-a0026c271264.herokuapp.com/']
 
 
 # Application definition
@@ -77,6 +78,7 @@ INSTALLED_APPS = [
     'allauth.account', 
     'allauth.socialaccount', 
     'dj_rest_auth.registration',
+    'corsheaders',
 
     'profiles',
     'posts',
@@ -87,6 +89,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -95,6 +98,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    origin for origin in [
+        os.environ.get('CLIENT_ORIGIN'),
+        os.environ.get('CLIENT_ORIGIN_DEV')
+    ] if origin
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'book_reviews_api.urls'
 
